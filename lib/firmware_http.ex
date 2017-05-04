4d426255dcb7562f0f00d75fc1a815bc8d9e6aa1
Justin Schneck <jschneck@mac.com>
2017-05-04 10:49:41 -0400

    allow custom json library to be used as well as custom encode opts (#8)
    * allow custom json library to be used as well as custom encode opts
    
    * make exjsx optional

--- lib/firmware_http.ex
+++ lib/firmware_http.ex
@@ -20,13 +20,17 @@ defmodule Nerves.Firmware.HTTP do
   | :port   | 8988                 |                                     |
   | :path   | "/firmware"          |                                     |
   | :stage_file | "/tmp/uploaded.fw"   | Firmware will be uploaded here before install, and deleted afterward |
+  | :json_provider | JSX          |
+  | :json_opts     | []           | 
   | :timeout       | 120000       |
 
   So, for instance, in your config.exs, you might do:
 
         config :nerves_firmware_http, port: 9999,
                                       path: "/services/firmware",
-                                      stage_file: "/my_tmp/new.fw",
+                                      stage_file: "/my_tmp/new.fw"
+                                      json_provider: Poison,
+                                      json_opts: [space: 1, indent: 2]
                                       timeout: 240_000
   """
   @doc "Application start callback"
--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Nerves.Firmware.HTTP.Mixfile do
   defp deps do
     [{:nerves_firmware, github: "nerves-project/nerves_firmware"},
      {:cowboy, "~> 1.0"},
-     {:exjsx, "~> 4.0"},
+     {:exjsx, "~> 4.0", optional: true},
      {:ex_doc, "~> 0.15", only: :dev},
      {:httpotion, "~> 3.0", only: :test}]
   end
